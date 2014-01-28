7dd5e17023dc67c75349f84cc6d75ee77bebaa2c
Noel Cower <ncower@gmail.com>
2014-01-28 03:22:28 -0800

    Add QAppDelegate.
    This'll cover font changing notifications and such.

---
+++ Schemer/QAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  QAppDelegate.h
+//  Schemer
+//
+//  Created by Noel Cower on 01/28/14.
+//  Copyright (c) 2014 Spifftastic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+extern NSString *const QFontChangeNotification;
+
+
+@interface QAppDelegate : NSObject <NSApplicationDelegate>
+
+@end
---
+++ Schemer/QAppDelegate.m
@@ -0,0 +1,36 @@
+//
+//  QAppDelegate.m
+//  Schemer
+//
+//  Created by Noel Cower on 01/28/14.
+//  Copyright (c) 2014 Spifftastic. All rights reserved.
+//
+
+#import "QAppDelegate.h"
+
+
+NSString *const QFontChangeNotification = @"QUserFontChangedNotification";
+
+
+@implementation QAppDelegate {
+  NSFont *_font;
+}
+
+
+- (void)applicationWillFinishLaunching:(NSNotification *)notification
+{
+  _font = [NSFont userFixedPitchFontOfSize:0.0];
+  NSFontManager *manager = [NSFontManager sharedFontManager];
+  [manager setSelectedFont:_font isMultiple:NO];
+  manager.delegate = self;
+}
+
+
+- (void)changeFont:(id)sender
+{
+  _font = [sender convertFont:[sender convertFont:_font] toSize:11.0];
+  [NSFont setUserFixedPitchFont:_font];
+  [[NSNotificationCenter defaultCenter] postNotificationName:QFontChangeNotification object:self];
+}
+
+@end
